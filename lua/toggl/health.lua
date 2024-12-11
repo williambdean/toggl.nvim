5e9d129f3970d274695a4ade37066022a06d6c2f
Will Dean <wd60622@gmail.com>
2024-12-11 12:24:22 +0100

    add formatting and break out functions

--- lua/toggl/health.lua
+++ lua/toggl/health.lua
@@ -1,48 +1,60 @@
 local M = {}
 
 local function is_newer_version(version, min_version)
-	local major, minor, patch = version:match("(%d+)%.(%d+)%.(%d+)")
-	major, minor, patch = tonumber(major), tonumber(minor), tonumber(patch)
+  local major, minor, patch = version:match("(%d+)%.(%d+)%.(%d+)")
+  major, minor, patch = tonumber(major), tonumber(minor), tonumber(patch)
 
-	local min_major, min_minor, min_patch = min_version:match("(%d+)%.(%d+)%.(%d+)")
-	min_major, min_minor, min_patch = tonumber(min_major), tonumber(min_minor), tonumber(min_patch)
+  local min_major, min_minor, min_patch =
+    min_version:match("(%d+)%.(%d+)%.(%d+)")
+  min_major, min_minor, min_patch =
+    tonumber(min_major), tonumber(min_minor), tonumber(min_patch)
 
-	return major > min_major
-		or (major == min_major and minor > min_minor)
-		or (major == min_major and minor == min_minor and patch >= min_patch)
+  return major > min_major
+    or (major == min_major and minor > min_minor)
+    or (major == min_major and minor == min_minor and patch >= min_patch)
 end
 
-local function has_toggl()
-	local handle = io.popen("which toggl")
-	local result = handle:read("*a")
-	handle:close()
+function M.has_toggl_cli()
+  local handle = io.popen("which toggl")
+  local result = handle:read("*a")
+  handle:close()
 
-	return result ~= ""
+  return result ~= ""
 end
 
-function M.check_toggl_version()
-	local handle = io.popen("toggl --version")
-	local result = handle:read("*a")
-	handle:close()
+function M.has_toggl_api_token()
+  return os.getenv("TOGGL_API_TOKEN") ~= nil
+end
 
-	local min_version = "0.4.8"
-	return is_newer_version(result, min_version)
+function M.greater_than_480()
+  local handle = io.popen("toggl --version")
+  local result = handle:read("*a")
+  handle:close()
+
+  local min_version = "0.4.8"
+  return is_newer_version(result, min_version)
 end
 
 function M.check()
-	vim.health.start("Toggl.nvim")
-	if not has_toggl() then
-		vim.health.error("Toggl is not installed")
-		return
-	end
+  vim.health.start("Toggl.nvim")
+  if not M.has_toggl_cli() then
+    vim.health.error("Toggl is not installed")
+    return
+  end
 
-	vim.health.ok("Toggl is installed")
+  vim.health.ok("Toggl is installed")
 
-	if M.check_toggl_version() then
-		vim.health.ok("Toggl version check passed")
-	else
-		vim.heatth.info("Toggl version requires authentication with TogglAuth command")
-	end
+  if M.greater_than_480() and M.has_toggl_api_token() then
+    vim.health.ok("Toggl version check passed")
+  elseif M.greater_than_480() and not M.has_toggl_api_token() then
+    vim.health.warn(
+      "Set TOGGL_API_TOKEN environment variable to skip authentication"
+    )
+  else
+    vim.health.warn(
+      "Toggl version requires authentication with TogglAuth command"
+    )
+  end
 end
 
 return M
--- lua/toggl/init.lua
+++ lua/toggl/init.lua
@@ -3,120 +3,137 @@ local Job = require("plenary.job")
 local M = {}
 
 local partial = function(fn, ...)
-	local args = { ... }
-	return function(...)
-		return fn(unpack(args), ...)
-	end
+  local args = { ... }
+  return function(...)
+    return fn(unpack(args), ...)
+  end
 end
 
 function M.toggl_auth(get_token)
-	local token = get_token()
-	if token == "" then
-		vim.notify("Please provide get_token function to opts.")
-		return
-	end
+  local token = get_token()
+  if token == "" then
+    vim.notify("Please provide get_token function to opts.")
+    return
+  end
 
-	Job:new({
-		command = "toggl",
-		args = { "auth", token },
-		on_stdout = function(j, result)
-			vim.notify(result)
-		end,
-		on_stderr = function(j, result)
-			vim.notify(result, "error")
-		end,
-	}):start()
+  Job:new({
+    command = "toggl",
+    args = { "auth", token },
+    on_stdout = function(_, result)
+      vim.notify(result)
+    end,
+    on_stderr = function(_, result)
+      vim.notify(result, vim.log.levels.ERROR)
+    end,
+  }):start()
+end
+
+local function surrounded(str, char)
+  return str:sub(1, 1) == char and str:sub(-1) == char
+end
+
+local function remove_surrounding_quotes(str)
+  if surrounded(str, '"') or surrounded(str, "'") then
+    return str:sub(2, -2)
+  end
+
+  return str
 end
 
 function M.toggl_start(opts)
-	local description = opts.args
+  local description = opts.args
+  description = remove_surrounding_quotes(description)
+  if #description == 0 then
+    vim.notify("Please provide a description")
+    return
+  end
 
-	if #description == 0 then
-		vim.notify("Please provide a description")
-		return
-	end
-	Job:new({
-		command = "toggl",
-		args = { "start", description },
-		on_stdout = function(j, result)
-			vim.notify(result)
-		end,
-		on_stderr = function(j, result)
-			vim.notify(result, "error")
-		end,
-	}):start()
+  Job:new({
+    command = "toggl",
+    args = { "start", description },
+    on_stdout = function(_, result)
+      vim.notify(result)
+    end,
+    on_stderr = function(_, result)
+      vim.notify(result, vim.log.levels.ERROR)
+    end,
+  }):start()
 end
 
 function M.toggl_config()
-	Job:new({
-		command = "toggl",
-		args = { "config", "--path" },
-		on_stdout = function(j, result)
-			local path = vim.trim(result)
-			if path == "" then
-				return
-			end
+  Job:new({
+    command = "toggl",
+    args = { "config", "--path" },
+    on_stdout = function(_, result)
+      local path = vim.trim(result)
+      if path == "" then
+        return
+      end
 
-			if not path:match("%.toml$") then
-				vim.notify("Invalid config path", "error")
-				return
-			end
-			vim.notify("Config path: " .. path)
-			vim.schedule(function()
-				vim.cmd("edit " .. vim.fn.fnameescape(path))
-			end)
-		end,
-		on_stderr = function(j, result)
-			vim.notify(result, "error")
-		end,
-		on_exit = function(j, code)
-			if code ~= 0 then
-				vim.notify("Failed to get config path", "error")
-			end
-		end,
-	}):start()
+      if not path:match("%.toml$") then
+        vim.notify("Invalid config path", vim.log.levels.ERROR)
+        return
+      end
+      vim.notify("Config path: " .. path)
+      vim.schedule(function()
+        vim.cmd("tabnew " .. vim.fn.fnameescape(path))
+      end)
+    end,
+    on_stderr = function(_, result)
+      vim.notify(result, vim.log.levels.ERROR)
+    end,
+    on_exit = function(_, code)
+      if code ~= 0 then
+        vim.notify("Failed to get config path", vim.log.levels.ERROR)
+      end
+    end,
+  }):start()
 end
 
 function M.toggl_current()
-	Job:new({
-		command = "toggl",
-		args = { "current" },
-		on_stdout = function(j, result)
-			vim.notify(result)
-		end,
-		on_stderr = function(j, result)
-			vim.notify(result, "error")
-		end,
-	}):start()
+  Job:new({
+    command = "toggl",
+    args = { "current" },
+    on_stdout = function(_, result)
+      vim.notify(result)
+    end,
+    on_stderr = function(_, result)
+      vim.notify(result, "error")
+    end,
+  }):start()
 end
 
 function M.toggl_stop()
-	Job:new({
-		command = "toggl",
-		args = { "stop" },
-		on_stdout = function(j, result)
-			vim.notify(result)
-		end,
-		on_stderr = function(j, result)
-			vim.notify(result)
-		end,
-	}):start()
+  Job:new({
+    command = "toggl",
+    args = { "stop" },
+    on_stdout = function(_, result)
+      vim.notify(result)
+    end,
+    on_stderr = function(_, result)
+      vim.notify(result)
+    end,
+  }):start()
 end
 
 function M.setup(opts)
-	M.config = opts or {}
-	opts.get_token = opts.get_token or function()
-		return ""
-	end
+  M.config = opts or {}
+  opts.get_token = opts.get_token or function()
+    return ""
+  end
 
-	vim.api.nvim_create_user_command("TogglConfig", M.toggl_config, {})
-	vim.api.nvim_create_user_command("TogglStart", M.toggl_start, { nargs = "*" })
-	vim.api.nvim_create_user_command("TogglCurrent", M.toggl_current, {})
-	vim.api.nvim_create_user_command("TogglStop", M.toggl_stop, {})
-	if not health.check_toggl_version() then
-		return
-	end
-	vim.api.nvim_create_user_command("TogglAuth", partial(M.toggl_auth, opts.get_token), {})
+  vim.api.nvim_create_user_command("TogglConfig", M.toggl_config, {})
+  vim.api.nvim_create_user_command("TogglStart", M.toggl_start, { nargs = "*" })
+  vim.api.nvim_create_user_command("TogglCurrent", M.toggl_current, {})
+  vim.api.nvim_create_user_command("TogglStop", M.toggl_stop, {})
+  if health.greater_than_480() and health.has_toggl_api_token() then
+    return
+  end
+  vim.api.nvim_create_user_command(
+    "TogglAuth",
+    partial(M.toggl_auth, opts.get_token),
+    {}
+  )
 end
 
 return M
